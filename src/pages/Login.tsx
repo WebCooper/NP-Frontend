7eb45d8425cd4d4ec15a1f00289a0dd930a591e8
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-02-01 03:22:44 +0530

    login and register is ready

---
+++ src/pages/Login.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Login = () => {
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${baseUrl}/api/user/login`, { email, password });
+      toast.success('Login successful!');
+
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || 'Invalid credentials');
+      } else {
+        alert('An unexpected error occurred');
+      }
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-2xl w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder='Enter Your Email'
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder='Enter Your Password'
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg"
+          >
+            Teacher Login
+          </button>
+          <div className='pt-5'>
+            <h2>Don't have an account? <a href="/register" className="text-blue-600 hover:underline">Register</a></h2>
+          </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
